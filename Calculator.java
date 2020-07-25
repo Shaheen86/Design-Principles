66363a846798187988b9e9a012568b56febe6c54
Shaheen86 <Shaheen77886@gmail.com>
2020-07-25 20:39:38 +0530

    First Commit

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ public class Calculator{
 	public static int mult(int x,int y){
 		return(x*y);
 	}
-	public static int div(int x,int y){
+	public static double div(int x,int y){
 		return x/y;
 	}
 	public static void main(String args[]){
@@ -18,22 +18,22 @@ public class Calculator{
 		Calculator obj=new MainCalculator();
 		int x=sc.nextInt();
 		char operator = sc.next().charAt(0);
-		int b = sc.nextInt();
+		int y = sc.nextInt();
 		if(operator=='+'){
 			System.out.print("Addition of "+x+" and "+y+" is ");
-			System.out.println(obj.addition(x,y));
+			System.out.println(obj.add(x,y));
 		}
 		if(operator=='-'){
 			System.out.print("Subtraction of "+x+" and "+y+" is ");
-			System.out.println(obj.subtraction(x,y));
+			System.out.println(obj.subtract(x,y));
 		}
 		if(operator=='*'){
 			System.out.print("Multiplication of "+x+" and "+y +" is ");
-			System.out.println(obj.multiplication(x,y));
+			System.out.println(obj.mult(x,y));
 		}
 		if(operator=='/'){
 			System.out.print("Division of "+x+" and "+y+" is ");
-			System.out.println(obj.division(x,y));
+			System.out.println(obj.div(x,y));
 		}
 	}
 }
